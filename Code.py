6386bf7376ae8c13720a2445dc66a60bbaaea59d
Harshit Gajawada <115985931+HarshitGajawada@users.noreply.github.com>
2023-07-05 22:29:13 +0530

    Created beginning of code

---
+++ Code.py
@@ -0,0 +1,9 @@
+print()
+print("Welcome to the NearBy App.")
+print()
+print("1) Movies\n2) Hospital\n3) Eateries\n4) Petrol Station\n5) Banks")
+places=["Movies","Hospital", "Eateries", "Petrol Station", "Banks"]
+place=input("Where do you want to visit? ")
+while place not in places:
+    print("Please choose places from the options only.")
+    place=input("Where do you want to visit? ")
