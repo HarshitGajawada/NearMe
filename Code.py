2e3c96da5e102b9bde1bf9d70ae7d3544a4f13ac
The0therMe <yayavarampranav19@gmail.com>
2023-07-06 10:51:20 +0530

    added theatres data

--- Code.py
+++ Code.py
@@ -1,3 +1,5 @@
+import json
+
 print("Welcome to the NearBy App.\n\n")
 print("Where do you want to visit?\n")
 print("1 - Theatres\n2 - Hospitals\n3 - Eateries\n4 - Petrol Stations\n5 - Banks")
@@ -6,7 +8,14 @@ place=int(input("Press the corresponding number(1-5): "))
 if not 1 <= place <= 5:
     while not 1 <= place <= 5:
         place = int(input("enter valid number(1-5): "))
-        print("changes")
+
+
+if place==1:
+    with open('theatres.json','r') as theatres:
+        t = json.load(theatres)
+    for i in t["theatres"]:
+        print(i,"is",t["theatres"][i],"km away from you.\n")
+
         
         
     
